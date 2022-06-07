6c6d6f87d67d8a766959a791c57a6aec4452a028
mrparsaeian <m.r.parsa@gmail.com>
2022-06-07 13:16:45 +0430

    Upgrade webpack upgrade react-responsive-font-size

---
+++ src/modals/modals.jsx
@@ -0,0 +1,5 @@
+// import { ModalsProvider, useModals, ContextModalProps } from "@mantine/modals";
+// import {useModals } from "@mantine/modals";
+// import { Text, Button, TextInput, MantineProvider } from "@mantine/core";
+// export default  useModals();
+export {}
